9da39bcf3ff16819b035c6a46f7c04efc3dfb620
Piotr Serwatka <piotr.serwatka97@gmail.com>
2025-05-17 17:09:00 +0200

    delete endpoint.ts

--- src/services/api/endpoints.ts
+++
@@ -1,6 +0,0 @@
-const rickAndMotyAPIEndpoints = {
-    characters: 'https://rickandmortyapi.com/api/character',
-    locations: 'https://rickandmortyapi.com/api/location',
-}
-
-export default rickAndMotyAPIEndpoints;
